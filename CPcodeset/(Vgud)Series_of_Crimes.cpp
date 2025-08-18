f8357ce5de70daca58f58783dd2eaf8145eda6b2
LaZyMugen <shaswatsahoo234@gmail.com>
2025-08-18 15:53:25 +0530

    updated probset

--- CPcodeset/(Vgud)Series_of_Crimes.cpp
+++ CPcodeset/(Vgud)Series_of_Crimes.cpp
@@ -14,19 +14,19 @@ int main() {
 
     vector<int> row_count(n, 0), col_count(m, 0);
 
-    // Count stars in each row and column
+    
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if (grid[i][j] == '*') {
+            if (grid[i][j] == '1') {
                 row_count[i]++;
                 col_count[j]++;
+                
             }
         }
     }
 
     int req_row = -1, req_col = -1;
 
-    // Find row with exactly 1 star
     for (int i = 0; i < n; i++) {
         if (row_count[i] == 1) {
             req_row = i + 1; // 1-indexed
@@ -34,7 +34,6 @@ int main() {
         }
     }
 
-    // Find column with exactly 1 star
     for (int j = 0; j < m; j++) {
         if (col_count[j] == 1) {
             req_col = j + 1; // 1-indexed
